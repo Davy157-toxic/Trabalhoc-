b0f38d12439bcff461078d2ee299de132e50862c
Davy <machadodavi3778@gmail.com>
2024-11-07 12:09:19 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+public class n4
+{
+    public static void Main(string[] args)
+    {
+        int x = 12;
+        int y = 31;
+        int z = 4;
+        int j = 4;
+        if (x == y)
+        {
+            System.Console.WriteLine("comparando x e y e igual");
+        }
+        else
+        {
+            System.Console.WriteLine("x e y não e igual");
+        }
+        if (j == z)
+        {
+            System.Console.WriteLine("Comparando j e z são iguais");
+        }
+        else
+        {
+            System.Console.WriteLine("j e z não são iguais");
+        }
+        if (x == z)
+        {
+            System.Console.WriteLine("comparando x e z e igual");
+        }
+        else
+        {
+            System.Console.WriteLine("x e z não e igual");
+        }
+        if (x == j)
+        {
+            System.Console.WriteLine("comparando x e j e igual");
+        }
+        else
+        {
+            System.Console.WriteLine("x e j não e igual");
+
+        }
+    }
+}
+
+
